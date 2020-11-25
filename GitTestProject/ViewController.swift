73a8a37b06b40852a5bad0719b8003b1e3608e04
Cameron Barker <cameronbarkerqwerty@gmail.com>
2020-11-25 16:04:48 +0000

    Second commit

--- GitTestProject/ViewController.swift
+++ GitTestProject/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello world")
+        print("Hello again world")
     }
 
 
